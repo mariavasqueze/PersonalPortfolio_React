caa512446bfb62b844f1d52d9212da38314d7de2
Maria Vasquez <mariadelmarvze@gmail.com>
2022-12-23 19:58:03 -0800

    Projects Update with pictures and links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,11 +3,20 @@ import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project2-nasa-img.png";
 import projImg3 from "../assets/img/underConstruction.jpeg";
 import projImg4 from "../assets/img/homePage.png";
+import projImg5 from "../assets/img/mobileApp.jpeg";
 
 import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
 	const projects = [
+		{
+			title: "React Web App - Happy Hour Locator",
+			description:
+				"This is a team project. It works as a web app in which users will be able to find all the happy hours in Vancouver and filter by location. Users can join as a member to get special discounts and access to special day events in restaurants and bars. To test as user or admin please use credentials in Github repo.",
+			imageUrl: projImg4,
+			demo: "https://www.happyhourlocator.ca/",
+			code: "https://github.com/mariavasqueze/happy-hour-locator",
+		},
 		{
 			title: "React / Redux Online Store",
 			description:
@@ -21,24 +30,16 @@ const Projects = () => {
 			description:
 				"Connected the SpaceX API with a NASA mission control front-end to show all launches filtering the information to show which missions have been completed or aborted. All the data is saved on a MongoDB database. The user can also create & abort future missions to the Exoplanets from NASA’s data.",
 			imageUrl: projImg2,
-			demo: "https://github.com/mariavasqueze/NodeJS_API_Nasa-Spacex", // change this later
+			demo: "https://github.com/mariavasqueze/NodeJS_API_Nasa-Spacex",
 			code: "https://github.com/mariavasqueze/NodeJS_API_Nasa-Spacex",
 		},
 		{
-			title: "React Web App - Links will be activated by Dec 03",
+			title: "Java Mobile App - Job Post Skill Matcher",
 			description:
-				"Happy Hour Locator - This is a team project in development. It works as a web app in which users will be able to find all the happy hours in Vancouver and filter by location and time. Users can join as a member to get special discounts and access to special day events in restaurants and bars.",
-			imageUrl: projImg4,
-			demo: "/",
-			code: "/",
-		},
-		{
-			title: "Java Mobile App - Under Construction (Delivery Dec 05)",
-			description:
-				"Job skill comparison. This Java app calls the Indeed API and show results of jobs in a recycler view which users can compare their top 10 skills (saved on their profile) and see their match.",
-			imageUrl: projImg3,
-			demo: "/",
-			code: "/",
+				"Job skill comparison. This Java app calls a Job Posting (demo) API and shows results of jobs in a recycler view which users can compare their top skills (saved on DB) and see & save their matches.",
+			imageUrl: projImg5,
+			demo: "https://github.com/mariavasqueze/JavaMobileApp-JobPostSkillMatcher",
+			code: "https://github.com/mariavasqueze/JavaMobileApp-JobPostSkillMatcher",
 		},
 	];
 
